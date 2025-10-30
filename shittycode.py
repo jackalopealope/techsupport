f18e589b888540427bbe998d976f123d0a434274
Jack Hanni <theunwindaccord@gmail.com>
2025-10-30 09:29:50 -0600

    Minor additions

--- shittycode.py
+++ shittycode.py
@@ -14,7 +14,7 @@ global X
 #Query Strings
 q0="What is the first letter of the city that your school is located in?\n"
 q1="What grades are at this school? (e.g. for 3rd, 4th, 5th: 345)\n"
-q2=""
+q2="Who teaches history hijinks [Put last name only. For Ms. Example you would just put 'example']\n"
 q3=""
 q4=""
 q5=""
@@ -40,19 +40,22 @@ I = input(q0).capitalize()
 checklen(I,1)
 #Ensure variable is string
 if isinstance(I,str) != True:
-    print("Bad Input")
+    print("Bad Input or Incorrect Answer")
     input("\nPress enter key to exit")
     exit()
 
 II = input(q1)
+III = input(q3).capitalize
 #Ensure Length is correct
 checklen(II,3)
+checklen(III,6)
 #Convert Variables to integers for checksum generation
 #Convert I to its alphabetical index plus one so that it wont set the full checksum to 0 when multiplies
 I = string.ascii_uppercase.index(I)+1
 II = int(II)+1
+III = string.ascii_uppercase.index(III)+1
 #Development code only
-print((I,II))
+print((I,II,III))
 
     
 
